dbf7e4209b52097569e2dd6d4eb376d6ac5a8a63
Alex Bosworth <alex.bosworth@gmail.com>
2020-07-16 10:20:41 -0700

    update dependencies

--- test/fiat/test_get_historic_rate.js
+++ test/fiat/test_get_historic_rate.js
@@ -1,4 +1,4 @@
-const {test} = require('tap');
+const {test} = require('@alexbosworth/tap');
 
 const getHistoricRate = require('./../../fiat/get_historic_rate');
 
--- test/harmony/test_notes_for_chain_transaction.js
+++ test/harmony/test_notes_for_chain_transaction.js
@@ -1,4 +1,4 @@
-const {test} = require('tap');
+const {test} = require('@alexbosworth/tap');
 
 const notesForChainTx = require('./../../harmony/notes_for_chain_transaction');
 
